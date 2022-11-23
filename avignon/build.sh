b69cdc114dd40a891f96d352ae28ee3441198453
Jorge Lázaro Ruiz <jorgelazaro2002@hotmail.com>
2022-11-23 10:00:46 +0100

    added command for unit tests

--- avignon/build.sh
+++ avignon/build.sh
@@ -2,3 +2,4 @@
 export LD_LIBRARY_PATH=/opt/gcc-12.1.0/lib64
 cmake -S . -B release -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/g++
 cmake --build release
+ctest --test-dir release
