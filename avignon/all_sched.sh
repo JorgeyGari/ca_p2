ffb0ba9f978311c894d1a78b9ae971dc223548f0
Jorge Lázaro Ruiz <jorgelazaro2002@hotmail.com>
2022-11-24 16:22:50 +0100

    Update all_sched.sh

--- avignon/all_sched.sh
+++ avignon/all_sched.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 echo ---  STATIC SCHEDULING  ---
+export OMP_SCHEDULE=static
 sbatch all_thr.sh
 
 echo --- DYNAMIC SCHEDULING  ---
+export OMP_SCHEDULE=dynamic
 sbatch all_thr.sh
 
 echo ---  GUIDED SCHEDULING  ---
+export OMP_SCHEDULE=guided
 sbatch all_thr.sh
